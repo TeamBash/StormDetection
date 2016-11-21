77d1e1106bf1778983bd3d9f59c6ae31dff3cfa9
rraavi <raghuveer.raavi@gmail.com>
2016-11-21 00:20:43 -0500

    changes to before install and .travis

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -15,21 +15,3 @@ fi
 echo 'Removing existing docker instances' >> /var/log/sga-docker.log 2>&1
 docker ps -a | grep 'storm-detection-service' | awk '{print $1}' | xargs --no-run-if-empty docker stop
 docker ps -a | grep 'storm-detection-service' | awk '{print $1}' | xargs --no-run-if-empty docker rm
-
-install_dir="/usr/local"
-dir="/usr/local/zookeeper-3.4.8"
-if [ ! -d "$dir" ] ; then
-cd "$install_dir"
-wget http://www-us.apache.org/dist/zookeeper/zookeeper-3.4.8/zookeeper-3.4.8.tar.gz
-tar xzf zookeeper-3.4.8.tar.gz
-rm zookeeper-3.4.8.tar.gz
-cd "$dir/conf"
-touch zoo.cfg
-echo "tickTime=2000" > zoo.cfg
-echo "dataDir=/var/lib/zookeeper" >> zoo.cfg
-echo "clientPort=2181" >> zoo.cfg
-fi
-
-cd "$dir/bin"
-./zkServer.sh stop
-./zkServer.sh start
